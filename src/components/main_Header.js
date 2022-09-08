443f5ec9631de9a96bde4a90d5f519e5388a97da
Vitor <vitor.sesi21@gmail.com>
2022-09-08 20:10:57 -0300

    HeaderMainCassinoIniciado

---
+++ src/components/main_Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import '../assets/css/main_Header.css'
+
+export default function MainHeader() {
+    return (
+        <div>
+            <section className='row align centralize main_header_suport'>
+                <button className='main_header_item press_btn'>Double</button>
+                
+                <button className='main_header_item press_btn'>Histórico</button>
+
+
+            </section>
+        </div>
+    )
+}
--- src/pages/cassino/cassino.jsx
+++ src/pages/cassino/cassino.jsx
@@ -4,21 +4,21 @@ import '../../assets/css/cassino.css'
 import { useEffect } from 'react';
 import { render } from 'react-dom';
 import SideBar from '../../components/side_bar.js'
+import MainHeader from '../../components/main_Header';
 
 export default function Cassino() {
 
     const [Lista, setLista] = useState([]);
     const [Item, setItem] = useState('');
-    const [CountClick, setCountClick] = useState();
 
     return (
         <div className='row'>
-            <SideBar parentToChild={'2'}/>
+            <SideBar parentToChild={'2'} />
 
             <main id='main' className='column align centralize'>
-                <h1 id='cassino'>Cassino</h1>
-                <div id='cassino' className='block_home_suport column centralize align'>
-                    
+                <div id='cassino' className='cassino_suport_background column'>
+                    <MainHeader />
+
                 </div>
             </main>
         </div>
